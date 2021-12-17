56f97738e600c8b0596fe3eada0f4738a69b30ca
ManuelAvanzi <manuel.92.avanzi@gmail.com>
2021-12-17 17:12:12 +0100

    drawGrid

--- js/script.js
+++ js/script.js
@@ -2,13 +2,33 @@
 const btn1=document.getElementById("btn1");
 const btn2=document.getElementById("btn2");
 const btn3=document.getElementById("btn3");
+const gridContainer=document.querySelector(".gridcontainer");
 
 btn1.addEventListener("click",function(){
-    console.log("1-100");
+    drawGrid(100);
 });
 btn2.addEventListener("click",function(){
-    console.log("1-81");
+    drawGrid(81);
 })
 btn3.addEventListener("click",function(){
-    console.log("1-49");
+    drawGrid(49);
 })
+
+
+function drawGrid(num){
+
+    for(let i=0;i<num;i++){
+        
+        const newSquare=document.createElement('div');
+        newSquare.className="box";
+
+        if(num>=49){
+            newSquare.className="medium";
+        }else if(num>=81){
+            newSquare.className="hard";
+        }
+        
+        gridContainer.append(newSquare);
+
+    }
+}
