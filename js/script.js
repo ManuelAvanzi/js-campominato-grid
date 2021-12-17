d359a686a18de5c6cd5954940a8d2d2e4bee809c
ManuelAvanzi <manuel.92.avanzi@gmail.com>
2021-12-17 17:56:13 +0100

    drawGrid fixed

--- js/script.js
+++ js/script.js
@@ -5,30 +5,28 @@ const btn3=document.getElementById("btn3");
 const gridContainer=document.querySelector(".gridcontainer");
 
 btn1.addEventListener("click",function(){
-    drawGrid(100);
+    drawGrid(100,"easy");
 });
 btn2.addEventListener("click",function(){
-    drawGrid(81);
+    drawGrid(81,"medium");
 })
 btn3.addEventListener("click",function(){
-    drawGrid(49);
+    drawGrid(49,"hard");
 })
 
 
-function drawGrid(num){
+function drawGrid(num,difficulty){
 
     for(let i=0;i<num;i++){
-        
+      
+        //creo la box
         const newSquare=document.createElement('div');
+        //applico alla box lo stile di default 
         newSquare.className="box";
-
-        if(num>=49){
-            newSquare.className="medium";
-        }else if(num>=81){
-            newSquare.className="hard";
-        }
-        
-        gridContainer.append(newSquare);
+        //aggiungo alla box lo stile contenuto nella variabilie difficulty
+        newSquare.classList.add(difficulty);
+        // appendo tutto 
+        gridContainer.append(newSquare)
 
     }
 }
