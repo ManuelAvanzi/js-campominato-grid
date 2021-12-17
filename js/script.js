857f6ac7ad8ca8a747c0edfd6033a3248ea0a496
ManuelAvanzi <manuel.92.avanzi@gmail.com>
2021-12-17 16:00:39 +0100

    buttons

---
+++ js/script.js
@@ -0,0 +1,14 @@
+//creo le varibili dei pulsanti
+const btn1=document.getElementById("btn1");
+const btn2=document.getElementById("btn2");
+const btn3=document.getElementById("btn3");
+
+btn1.addEventListener("click",function(){
+    console.log("1-100");
+});
+btn2.addEventListener("click",function(){
+    console.log("1-81");
+})
+btn3.addEventListener("click",function(){
+    console.log("1-49");
+})
